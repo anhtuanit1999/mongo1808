88b06440c3e736bc632a3b0dbb848e057913a52b
AnhTuanIT <phamanhtuan24031999@gmail.com>
2020-02-15 21:22:06 +0700

    Done

--- Word.js
+++ Word.js
@@ -1,24 +1,17 @@
 const mongoose = require('mongoose');
-const URL = 'mongodb://localhost:27017/shop';
 
-mongoose.connect(URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true 
+const Schema = mongoose.Schema;
+const WordSchema = new Schema({
+    en: String,
+    vn: String
 });
 
-mongoose.connection.once('open', () => {
-    console.log('Hello');
-});
+const Word = mongoose.model('Word', WordSchema);
 
-// const Schema = mongoose.Schema;
-// const WordSchema = new Schema({
-//     en: String,
-//     vn: String
-// });
-
-// const Word = mongoose.model('Word', WordSchema);
 // const remote = new Word({ en: "remote", vn: "Điều khiển" });
 // remote.save().then((value) => {
 //     console.log(value);
 // });
+
+module.exports = Word;
 
--- server.js
+++ server.js
@@ -1,14 +1,58 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Word = require('./Word');
+const parser = require('body-parser').urlencoded({ extended: false });
 const app = express();
 const PORT = 3000;
 
+//**********************************************
 app.set('views', './views');
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.render('home'));
+app.get('/', (req, res) => {
+    Word.find()
+    .then(result => res.render('home', { arrWords: result }));
+});
 
+app.get('/remove/:id', (req, res) => {
+    const { id } = req.params;
+    Word.findByIdAndDelete(id)
+    .then(() => res.redirect('back'))
+    .catch(err => res.send(err.message));
+});
+
+app.get('/edit/:id', (req, res) => {
+    const { id } = req.params;
+    // Word.findOne({ _id: id})
+    // .then(docs =>  res.render('edit', { result: docs }))
+    // .catch(err => res.send(err.message));
+    Word.findById(id)
+    .then(result =>  res.render('edit', { result }))
+    .catch(err => res.send(err.message));
+});
+
+app.post('/add', parser, (req, res) => {
+    const { en, vn } = req.body;
+    // const objectInsert = { en, vn };
+    // Word.insertMany(objectInsert)
+    // .then(() => res.redirect('back'))
+    // .catch(err => res.send(err.message));
+    const word  = new Word({ en, vn });
+    word.save()
+    .then(() => res.redirect('back'))
+    .catch(err => res.send(err.message));
+});
+
+app.post('/edit/:id', parser, (req, res) => {
+    const { id } = req.params;
+    const { en, vn } = req.body;
+    const objectEdit = { en, vn };
+    Word.findByIdAndUpdate(id, objectEdit)
+    .then(() => res.redirect('/'))
+    .catch(err => res.send(err.message));
+});
+//**********************************************
 const URL = 'mongodb://localhost:27017/shop';
 
 mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true });
